509ff6ac0efcc19345d1797624ebf09d4aa98cfb
Vivek Murali <vivekmurali2k@gmail.com>
2022-03-23 02:38:39 +0530

    Fix #3

--- server/main.go
+++ server/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"tempbin/server/db"
 	"tempbin/server/handlers"
 	"time"
@@ -20,21 +22,42 @@ func main() {
 	// go cleaner()
 	s := gocron.NewScheduler(time.UTC)
 	var job func() = worker
-	s.Every(10).Minutes().Do(job)
+	s.Every(1).Minutes().Do(job)
 	s.StartAsync()
 
 	// middlewares
 	r.Use(middleware.Logger)
 
 	// routes
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "hello")
-	})
-	r.Get("/upload", handlers.UploadHandler)
+	r.Get("/", handlers.UploadHandler)
 	r.Post("/upload", handlers.Upload)
+
+	workDir, _ := os.Getwd()
+	filesDir := http.Dir(filepath.Join(workDir, "server/static"))
+	FileServer(r, "/static", filesDir)
+
 	r.Route("/download", func(r chi.Router) {
 		r.Get("/{url}", handlers.DownloadHandler)
 		r.Post("/{url}", handlers.Download)
 	})
 	http.ListenAndServe(":3000", r)
+}
+
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit any URL parameters.")
+	}
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		rctx := chi.RouteContext(r.Context())
+		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
+		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
+		fs.ServeHTTP(w, r)
+	})
 }
